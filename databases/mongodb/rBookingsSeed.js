c6e3ada03713c32f6b4c8fa7770e33b417851bba
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-07 19:13:16 -0800

    Begin updateBookings function

--- databases/mongodb/rBookingsSeed.js
+++ databases/mongodb/rBookingsSeed.js
@@ -9,15 +9,12 @@ const findBookings = (bProperty_ID, callback) => {
       console.log(`error finding the documents for${bProperty_ID}: `, err);
     } else {
       if (docs.length > 0) {
-        // let getDocs = new Date();
-        // console.log('Time to getDocs: ', getDocs - start);
         let bookingIDs = docs.map((doc) => {
           return doc['_id'];
         });
         let end = new Date();
+        // console.log(`booking_ids for${bProperty_ID}: `, bookingIDs);   
         console.log('Time to end: ', end - start);
-        console.log(`booking_ids for${bProperty_ID}: `, bookingIDs);
-        
       }
     }
   });
@@ -25,7 +22,6 @@ const findBookings = (bProperty_ID, callback) => {
 
 //const updateRooms(rID, )
 //findBookings(1);
-findBookings(10);
-
-
-
+for (var i = 1; i <= 1000; i++) {
+ findBookings(i);   
+}
